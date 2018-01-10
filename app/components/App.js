2ee311a2218009520a2ec6db06207ac516753ac8
David Skaggs <davidtskaggs@gmail.com>
2018-01-10 13:42:20 -0500

    Style and component update

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,11 @@
 var React = require('react');
+var Popular = require('./Popular');
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello React Training!
+      <div className="container">
+        <Popular />
       </div>
     )
   }
