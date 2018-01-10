6ff0911e0f97e11d9307b059074c4c98b12c730f
David Skaggs <davidtskaggs@gmail.com>
2018-01-10 14:05:15 -0500

    Add functionality and style to popular comp

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,22 @@
 var React = require('react');
 
 class Popular extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  updateLanguage(lang) {
+    this.setState(function() {
+      return {
+        selectedLanguage: lang
+      }
+    });
+  }
+
   render() {
     var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 
@@ -10,11 +26,13 @@ class Popular extends React.Component {
           {languages.map(function(lang) {
             return (
               <li
+                style={lang ===this.state.selectedLanguage ? { color: '#d0021b'}: null }
+                onClick={this.updateLanguage.bind(null, lang)}
                 key={lang}>
                 {lang}
               </li>
             )
-          })}
+          }, this)}
         </ul>
       </div>
     )
