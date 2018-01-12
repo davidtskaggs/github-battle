39e1e9b6535be1a5c1a1c68fa37f9fa842cbf1dc
David Skaggs <davidtskaggs@gmail.com>
2018-01-12 08:23:33 -0500

    Add reset functionality

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -99,12 +99,21 @@ class Battle extends React.Component {
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
   handleSubmit(id, username) {
     this.setState(function () {
       var newState = {};
       newState[id + 'Name'] = username;
       newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200'
+      return newState;
+    });
+  }
+  handleReset(id) {
+    this.setState(function () {
+      var newState = {};
+      newState[id + 'Name'] = '';
+      newState[id + 'Image'] = null;
       return newState;
     });
   }
