7ef165d13ac9c29ff8195040eef9127771bff38e
David Skaggs <davidtskaggs@gmail.com>
2018-01-12 12:28:57 -0500

    Add variables for github api key

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,10 @@
 var axios = require('axios');
 
+// In case of permission denied errors
+// var id = "YOUR_CLIENT_ID";
+// var sec = "YOUR_SECRET_ID";
+// var params = "?client_id" + id + "&client_secret=" + sec;
+
 module.exports = {
   fetchPopularRepos: function (language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
