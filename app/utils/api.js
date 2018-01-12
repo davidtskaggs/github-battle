7fac14c5d22d5d6d71eb952e2c5a9cc9c626f86c
David Skaggs <davidtskaggs@gmail.com>
2018-01-12 12:32:46 -0500

    Add getProfile function

--- app/utils/api.js
+++ app/utils/api.js
@@ -5,7 +5,19 @@ var axios = require('axios');
 // var sec = "YOUR_SECRET_ID";
 // var params = "?client_id" + id + "&client_secret=" + sec;
 
+function getProfile(username) {
+  return axios.get('https://api.github.com/users/' + username + params)
+    .then(function(user) {
+      return user.data;
+    });
+}
+
+getProfile('')
+
 module.exports = {
+  battle: function (players) {
+
+  },
   fetchPopularRepos: function (language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
 
