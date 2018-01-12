8753b5ac0fd06341f32b537dd7b1becf8c7c12d9
David Skaggs <davidtskaggs@gmail.com>
2018-01-12 12:48:21 -0500

    Add helper functions

--- app/utils/api.js
+++ app/utils/api.js
@@ -55,9 +55,12 @@ function sortPlayers() {
   });
 }
 
+
 module.exports = {
   battle: function (players) {
-
+    return axios.all(players.map(getUserData))
+      .then(sortPlayers)
+      .catch(handleError)
   },
   fetchPopularRepos: function (language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
