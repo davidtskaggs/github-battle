c0ea92e8286e2d91c76e8aadbd8f6ed2a936ca8f
David Skaggs <davidtskaggs@gmail.com>
2018-01-15 09:14:27 -0500

    Add loading component to results

--- app/components/Results.js
+++ app/components/Results.js
@@ -4,6 +4,8 @@ var queryString = require('query-string');
 var api = require('../utils/api');
 var Link = require('react-router-dom').Link;
 var PlayerPreview = require('./PlayerPreview');
+var loading = require('./Loading')
+
 
 function Profile (props) {
   var info = props.info;
@@ -86,7 +88,7 @@ class Results extends React.Component {
     var loading = this.state.loading;
 
     if (loading === true) {
-      return <p>Loading!</p>
+      return <Loading />
     }
 
     if (error) {
