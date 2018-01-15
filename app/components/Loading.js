e90215f6f3d8592b10f9dba1996c6df7c40c5efe
David Skaggs <davidtskaggs@gmail.com>
2018-01-15 09:07:13 -0500

    Add loading component

---
+++ app/components/Loading.js
@@ -0,0 +1,12 @@
+var react = require('react');
+var PropTypes = require('prop-types');
+
+class Loading extends React.Component {
+  render() {
+    return (
+
+    )
+  }
+}
+
+module.exports = Loading;
