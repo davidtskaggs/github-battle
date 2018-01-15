2b47a9e4fff6cd0690994813ef1fe9d95e7600e8
David Skaggs <davidtskaggs@gmail.com>
2018-01-15 09:12:00 -0500

    Add to loading component

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,12 +1,35 @@
 var react = require('react');
 var PropTypes = require('prop-types');
 
+var styles = {
+  content: {
+    textAlign: 'center',
+    fontSize: '35px'
+  }
+};
+
 class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: props.text
+    };
+  }
   render() {
     return (
-
+      <p style={styles.content}>
+        {this.state.text}
+      </p>
     )
   }
+}
+loading.propTypes = {
+  text: PropTypes.string.isRequired
+}
+
+loading.defaultProps = {
+  text: 'Loading'
 }
 
 module.exports = Loading;
